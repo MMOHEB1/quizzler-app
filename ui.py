38871e2e16269ef7f6f6b2d489852365ccc14c37
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-07-22 15:45:31 +0100

    commit 4

--- ui.py
+++ ui.py
@@ -14,13 +14,21 @@ class QuizInterface:
         self.label.grid(row=0, column=1)
 
         self.canvas = Canvas(height=250, width=300,bg="white")
-        self.canvas.grid(row=1, column=0, columnspan=2)
-        self.question_text = self.canvas.create_text(150, 125, text="Question goes here", fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Question goes here",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
 
-        self.true_button = Button(image=PhotoImage(file="images/true.png"))
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
         self.true_button.grid(row= 2, column=0)
 
-        self.false_button = Button(image=PhotoImage(file="images/false.png"))
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
 
