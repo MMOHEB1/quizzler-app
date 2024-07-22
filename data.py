07ed0ea304b22e1ccbe96ffdf6a6fe8827e52366
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-07-22 14:41:16 +0100

    Used the html module imported in the quiz_brain file to unescape symbols that can be mistaken for html signs to allow for better user experience.

--- data.py
+++ data.py
@@ -5,12 +5,9 @@ parameters = {
     "type" : "boolean"
 }
 
-responce = requests.get(url="https://opentdb.com/api.php?", params=parameters)
-responce.raise_for_status()
-question_data = responce.json()["results"]
-print(question_data)
-
-
+response = requests.get(url="https://opentdb.com/api.php?", params=parameters)
+response.raise_for_status()
+question_data = response.json()["results"]
 
 
 # question_data = [
