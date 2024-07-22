3d0dfe0f46a326513bf805a9189d27b3f39f7ec4
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-07-22 15:38:09 +0100

    commit 3

--- ui.py
+++ ui.py
@@ -8,11 +8,18 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizmaster")
-        self.canvas = Canvas()
+        self.window.config(background=THEME_COLOR, padx=20, pady=20)
+
+        self.label = Label(text=f"Score: 0", fg="white", font=("Helvetica", 16), bg=THEME_COLOR)
+        self.label.grid(row=0, column=1)
+
+        self.canvas = Canvas(height=250, width=300,bg="white")
         self.canvas.grid(row=1, column=0, columnspan=2)
-        self.canvas.config(background=THEME_COLOR)
+        self.question_text = self.canvas.create_text(150, 125, text="Question goes here", fill=THEME_COLOR)
+
         self.true_button = Button(image=PhotoImage(file="images/true.png"))
         self.true_button.grid(row= 2, column=0)
+
         self.false_button = Button(image=PhotoImage(file="images/false.png"))
         self.false_button.grid(row=2, column=1)
 
